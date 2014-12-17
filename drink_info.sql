28a3b9e36b385d89ba08bc48db7ba1b63fd87b36
ed729136 <perezedwin991@gmail.com>
2014-12-17 14:16:00 -0500

    Some queries to go with the headfirst sql database

---
+++ drink_info.sql
@@ -0,0 +1,8 @@
+#SELECT * FROM headfirst.drink_info;
+#SELECT cost FROM headfirst.drink_info WHERE ice = 'y' AND color = 'yellow' AND calories > 33;
+#SELECT drink_name, color FROM headfirst.drink_info WHERE carbs < 4 AND ice = 'y';
+#SELECT cost FROM headfirst.drink_info WHERE CALORIES >= 80;
+#SELECT drink_name, color, ice FROM drink_info WHERE drink_name >= 'g' AND ( color = 'purple' OR color = 'yellow' );
+#Page 107, BETWEEN keyword
+#SELECT drink_name FROM drink_info WHERE drink_name BETWEEN 'g' AND 'p';
+
